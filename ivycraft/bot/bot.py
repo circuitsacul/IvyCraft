bdd49433a30c1a5a23aadb3f2eda8bb4ded33380
Circuit <circuitsacul@icloud.com>
2022-04-23 13:16:37 -0400

    Update bot, create commands

--- ivycraft/bot/bot.py
+++ ivycraft/bot/bot.py
@@ -4,16 +4,23 @@ import crescent
 
 from ivycraft.config import CONFIG
 from ivycraft.database.database import IvyDB
+from ivycraft.server.server import MCServer
 
 
 class Bot(crescent.Bot):
     def __init__(self) -> None:
-        super().__init__(CONFIG.discord_token)
+        super().__init__(
+            CONFIG.discord_token, default_guild=CONFIG.ivycraft_guild
+        )
 
         self.db = IvyDB()
+        self.server = MCServer()
+
+        self.plugins.load("ivycraft.bot.commands")
 
     async def start(self, *args: Any, **kwargs: Any) -> None:
         await self.db.connect()
+        await self.server.start()
         await super().start(*args, **kwargs)
 
 
---
+++ ivycraft/bot/commands.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+import crescent
+import hikari
+
+from ivycraft.config import CONFIG
+from ivycraft.database.models.user import User
+
+if TYPE_CHECKING:
+    from .bot import Bot
+
+
+async def guild_only(ctx: crescent.Context) -> None | crescent.HookResult:
+    if ctx.guild_id is None:
+        await ctx.respond(
+            "This command can only be used in a guild.", ephemeral=True
+        )
+        return crescent.HookResult(exit=True)
+
+    return None
+
+
+async def mc_mod(ctx: crescent.Context) -> None | crescent.HookResult:
+    ret = await guild_only(ctx)
+    if ret is not None:
+        return ret
+    assert ctx.member
+    if CONFIG.required_role not in ctx.member.role_ids:
+        await ctx.respond("You cannot run this command.", ephemeral=True)
+        return crescent.HookResult(exit=True)
+
+    return None
+
+
+plugin = crescent.Plugin("commands")
+
+
+@plugin.include
+@crescent.hook(mc_mod)
+@crescent.command(name="whitelist", description="Whitelist a player.")
+class WhitelistUser:
+    user = crescent.option(hikari.User, "The user to whitelist.")
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        user = await User.exists(discord_id=self.user.id)
+        if not user:
+            user = await User(
+                discord_id=self.user.id, whitelisted=True
+            ).create()
+
+        else:
+            user.whitelisted = True
+            await user.save()
+
+        await ctx.respond(f"{self.user} has been whitelisted.")
+
+
+@plugin.include
+@crescent.hook(mc_mod)
+@crescent.command(name="unwhitelist", description="Unwhitelist a player.")
+class UnwhitelistUser:
+    user = crescent.option(hikari.User, "The user to unwhitelist.")
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        user = await User.exists(discord_id=self.user.id)
+        if (not user) or user.whitelisted is False:
+            await ctx.respond(f"{self.user} is not whitelisted.")
+            return
+
+        user.whitelisted = False
+        await user.save()
+
+        await ctx.respond(f"{self.user} has been unwhitelisted.")
+
+
+@plugin.include
+@crescent.hook(guild_only)
+@crescent.command(
+    name="connect", description="Link your discord to your minecraft account."
+)
+class LinkAccounts:
+    name = crescent.option(
+        str, "Your Minecraft username.", name="minecraft-name"
+    )
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        bot = cast("Bot", ctx.app)
+        assert ctx.member is not None
+
+        user = await User.exists(discord_id=ctx.member.id)
+        if user is None or not user.whitelisted:
+            await ctx.respond("You are not whitelisted.", ephemeral=True)
+            return
+
+        if user.minecraft_uuid:
+            await ctx.respond(
+                "You are already linked to a minecraft account.",
+                ephemeral=True,
+            )
+            return
+
+        await bot.server.whitelist_user(user, self.name)
+        await ctx.respond(
+            "You've linked your Minecraft account.", ephemeral=True
+        )
