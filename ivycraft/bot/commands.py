a83e1d81fa25ce9308b835317112fa236fddcecd
Circuit <circuitsacul@icloud.com>
2022-04-24 14:37:41 -0400

    Add whois command

--- ivycraft/bot/commands.py
+++ ivycraft/bot/commands.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, cast
 
 import crescent
+import hikari
 
 from ivycraft.config import CONFIG
 from ivycraft.database.models.user import User
@@ -34,6 +35,78 @@ async def mc_mod(ctx: crescent.Context) -> None | crescent.HookResult:
 
 
 plugin = crescent.Plugin("commands")
+
+
+@plugin.include
+@crescent.hook(guild_only)
+@crescent.command(name="whois", description="Wait who's that")
+class Whois:
+    discord_user = crescent.option(
+        hikari.User, description="The Discord user to lookup.", default=None
+    )
+    minecraft_user = crescent.option(
+        str, description="The Minecraft username to lookup.", default=None
+    )
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        bot = cast("Bot", ctx.app)
+
+        if self.discord_user is not None:
+            user = await User.exists(id=self.discord_user.id)
+            if user is None or user.minecraft_uuid is None:
+                await ctx.respond(
+                    f"{self.discord_user} has not linked their minecraft "
+                    "account.",
+                    ephemeral=True,
+                )
+                return
+            minecraft_user = await bot.mojang.get_username(user.minecraft_uuid)
+            if minecraft_user is None:
+                await ctx.respond(
+                    f"{self.discord_user} is linked to an invalid account.",
+                    ephemeral=True,
+                )
+                return
+            await ctx.respond(
+                f"{self.discord_user}'s Minecraft username is "
+                f"{minecraft_user}."
+            )
+
+        elif self.minecraft_user is not None:
+            minecraft_uuid = await bot.mojang.get_uuid(self.minecraft_user)
+            if minecraft_uuid is None:
+                await ctx.respond(
+                    f"{self.minecraft_user} is not a valid Minecraft "
+                    "username.",
+                    ephemeral=True,
+                )
+                return
+            user = await User.exists(minecraft_uuid=minecraft_uuid)
+            if user is None:
+                await ctx.respond(
+                    f"{self.minecraft_user} is not linked to a Discord "
+                    "account.",
+                    ephemeral=True,
+                )
+                return
+            discord_user = await bot.rest.fetch_user(user.discord_id)
+            if discord_user is None:
+                await ctx.respond(
+                    f"{self.minecraft_user} is linked to an invalid account.",
+                    ephemeral=True,
+                )
+                return
+            await ctx.respond(
+                f"{self.minecraft_user}'s Discord account is {discord_user}.",
+                ephemeral=True,
+            )
+
+        else:
+            await ctx.respond(
+                "You must provide either a Discord user or a Minecraft "
+                "username.",
+                ephemeral=True,
+            )
 
 
 @plugin.include
